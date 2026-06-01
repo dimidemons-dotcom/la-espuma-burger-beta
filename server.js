c672cc7fc707e543a67c8cfa952cfe8fedb7ccf2
unknown <jesusitokitkat@gmail.com>
2026-06-01 18:20:47 +0200

    Fix frontend and admin menu sync

--- server.js
+++ server.js
@@ -272,11 +272,11 @@ app.get('/admin', (req, res) => {
 
 // Serve frontend app
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'app', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/app', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'app', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start server
